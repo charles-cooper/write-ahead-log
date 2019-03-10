aa8dd2818f2db806555f8c9d10110e84ff1ff0ee
Charles Cooper <cooper.charles.m@gmail.com>
2019-03-09 16:12:20 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+        name='write-ahead-log',
+        license='BSD3',
+        packages=['wal'],
+        python_requires='>=3.6',
+        author_email='cooper.charles.m@gmail.com',
+)
+
+
